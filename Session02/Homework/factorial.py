cbe54aa967394705456ff5ee3ff1dd7763e93222
someone <someone@someplace.com>
2017-11-02 22:34:51 +0700

    nop bai session02

--- Session02/Homework/factorial.py
+++ Session02/Homework/factorial.py
@@ -1,5 +1,8 @@
 n = int(input("Nhap so n: "))
 f = 1
-for i in range(1, n+1):
-    f = f * i
-print("Giai thua cua n la: ", f)
+if n > 0:
+    for i in range(1, n+1):
+        f = f * i
+    print("Giai thua cua n la: ", f)
+else:
+    print("Vui long nhap lai so nguyen duong")
